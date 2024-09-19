6dd170b0250876bacd8d348803251ffbe6c16f13
AdarshaCoding <adarshapc4467@gmail.com>
2024-09-19 19:42:49 +0530

    Different Request Handlers

--- src/app.js
+++ src/app.js
@@ -2,41 +2,27 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/user", rH1, rH2, [rH3, rH4], rH5, rH6)
-// we can wrap route Handlers in array, it works in the same way
-app.use(
-  "/user",
-  (req, res, next) => {
-    console.log("First Request Handler");
-    //res.send("Route Handler Function 1");
-    next();
-  },
-  (req, res, next) => {
-    console.log("2nd Request Handler");
-    //res.send("2nd Route Handler");
-    next();
-  },
-  (req, res, next) => {
-    console.log("3rd Request Handler");
-    //res.send("3rd Route Handler");
-    next();
-  },
-  (req, res, next) => {
-    console.log("4th Request Handler");
-    // res.send("4th Route Handler");
-    next(); // express will expect one more route handler here which sends the response back
-  },
-  (req, res) => {
-    console.log("5th Request Handler");
-    res.send("5th Response!");
-  }
-);
+// Authorization is duplicated
 
-/**
- * We should not res.send two times within nested or within the same request handler,
- * it cannot handle because TCP connection must have close after receiving the first response
- * we can add n number of route handler
- */
+app.get("/admin/getAllUsers", (req, res) => {
+  const token = "xyzz";
+  const isAdminAuthorized = token === "xyz";
+  if (!isAdminAuthorized) {
+    res.status(401).send("Not authorized!");
+  } else {
+    res.send("All users data!");
+  }
+});
+
+app.post("/admin/addUser", (req, res) => {
+  const token = "xyz";
+  const isAdminAuthorized = token === "xyz";
+  if (!isAdminAuthorized) {
+    res.status(401).send("Not authorized!");
+  } else {
+    res.send("One user is added!");
+  }
+});
 
 //listen
 app.listen(4000, () => {
